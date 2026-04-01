585827f5d9c024f03cd2907ae6b998b787485cd5
MChanda17 <meerachanda14@gmail.com>
2026-03-31 20:56:53 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -22,3 +22,14 @@ SELECT date,
 FROM us_counties_daily
 GROUP BY date
 ORDER BY date;
+
+-- Q4: Top 10 counties by death rate (latest date)
+SELECT county, state,
+       cases,
+       deaths,
+       ROUND((deaths * 1.0 / cases) * 100, 2) AS death_rate_percent
+FROM us_counties_daily
+WHERE date = (SELECT MAX(date) FROM us_counties_daily)
+  AND cases > 1000
+ORDER BY death_rate_percent DESC
+LIMIT 10;
